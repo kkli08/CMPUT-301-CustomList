2a5b89a4cd62f7b8d5eb251ba9b3e0906a4aae80
kkli08 <ke_2021@126.com>
2021-11-01 15:02:24 -0600

    run addCityTest

---
+++ app/src/test/java/com/example/simpleparadox/listcity/CustomListTest.java
@@ -0,0 +1,37 @@
+package com.example.simpleparadox.listcity;
+
+import static org.junit.jupiter.api.Assertions.assertEquals;
+
+import com.example.simpleparadox.listycity.City;
+import com.example.simpleparadox.listycity.CustomList;
+
+import org.junit.jupiter.api.BeforeAll;
+import org.junit.jupiter.api.BeforeEach;
+import org.junit.jupiter.api.Test;
+
+import java.util.ArrayList;
+
+public class CustomListTest {
+    private CustomList list;
+
+    @BeforeEach
+    public void creatList(){
+        list = new CustomList(null, new ArrayList<City>());
+    }
+
+    @Test
+    public void addCityTest(){
+        int listSize = list.getCount();
+        list.addCity(new City("Halifax", "NS"));
+        assertEquals(list.getCount(), listSize+1);
+    }
+
+    @Test
+    public void delete(City city){
+        int listSize = list.getCount();
+        list.delete(city);
+        assertEquals(list.getCount(), listSize-1);
+
+    }
+
+}
